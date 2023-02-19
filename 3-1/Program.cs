e130a5e806ba147505be16150d6e0b775fc28c04
Elya Fileva <filevaelya@mail.ru>
2023-02-19 11:12:10 +0300

    Добавлена задача 3-1

---
+++ 3-1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int[] array = { 1, 1, 1, 1, 0 };
+bool num = true;
+int i = 0;
+int j = array.Length - 1;
+Console.Write("число является палиндромом: ");
+while (i <= j)
+{
+  if (array[i] == array[j])
+  {
+    i++;
+    j--;
+  }
+  else
+  {
+    num = !num;
+    break;
+  }
+}
+Console.WriteLine(num);
